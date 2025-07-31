bfa16bcf79a56661548a24b4fe78d8a1bb274055
cprosko <cprosko@gmail.com>
2025-07-31 11:11:40 +0200

    Adjusted MapBlock to be Node2D with TileMapLayers as children, to allow multiple layers per block.

--- scripts/map_block.gd
+++ scripts/map_block.gd
@@ -1,5 +1,5 @@
 class_name MapBlock
-extends TileMapLayer
+extends Node2D
 
 # Public Parameters
 @export_category("Neighbors")
@@ -11,15 +11,31 @@ extends TileMapLayer
 # Public Properties
 @onready var BlockSizeTiles: Vector2i:
 	get:
-		return get_used_rect().size
+		return _block_size_tiles
 
+# Public Variables
+@onready var map_layers: Array = find_children("*", "TileMapLayer")
+
+# Private Variables
+var _block_size_tiles: Vector2i
 
 # Built-in Method Overrides
 func _ready() -> void:
+	_calculate_block_size()
 	_validate_neighbors()
 
 
 # Private Methods
+func _calculate_block_size() -> void:
+	var used_rects: Array[Rect2i] = map_layers.map(
+		func(x): return x.get_used_rect()
+	)
+	var full_rect: Rect2i = used_rects[0]
+	for rect in used_rects.slice(1):
+		full_rect.merge(rect)
+	_block_size_tiles = full_rect.size
+
+
 func _validate_neighbors() -> void:
 	# TODO: fill in
 	return
