8b04ec2dccb7762b5380f51ebb84d8c007883a48
cprosko <cprosko@gmail.com>
2025-07-31 11:15:15 +0200

    Added BlockRect property to MapBlock.

--- scripts/map_block.gd
+++ scripts/map_block.gd
@@ -9,7 +9,10 @@ extends Node2D
 @export var neighbor_right: MapBlock
 
 # Public Properties
-@onready var BlockSizeTiles: Vector2i:
+var BlockRect: Rect2i:
+	get:
+		return _block_rect
+var BlockSizeTiles: Vector2i:
 	get:
 		return _block_size_tiles
 
@@ -17,23 +20,24 @@ extends Node2D
 @onready var map_layers: Array = find_children("*", "TileMapLayer")
 
 # Private Variables
+var _block_rect: Rect2i
 var _block_size_tiles: Vector2i
 
 # Built-in Method Overrides
 func _ready() -> void:
-	_calculate_block_size()
+	_calculate_block_rect()
 	_validate_neighbors()
 
 
 # Private Methods
-func _calculate_block_size() -> void:
+func _calculate_block_rect() -> void:
 	var used_rects: Array[Rect2i] = map_layers.map(
 		func(x): return x.get_used_rect()
 	)
-	var full_rect: Rect2i = used_rects[0]
+	_block_rect = used_rects[0]
 	for rect in used_rects.slice(1):
-		full_rect.merge(rect)
-	_block_size_tiles = full_rect.size
+		_block_rect.merge(rect)
+	_block_size_tiles = _block_rect.size
 
 
 func _validate_neighbors() -> void:
