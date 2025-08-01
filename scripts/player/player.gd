1445ef4e09950dc17b81cede380415bf69476e5b
cprosko <cprosko@gmail.com>
2025-08-01 20:59:24 +0200

    Added animations to Player.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -15,6 +15,9 @@ var TilePosition: Vector2i:
 	set(val):
 		_tile_position = val
 
+# Cached References
+@onready var sprite = $Sprite
+
 # Private Variables
 var _movement_tween: Tween
 var _tile_position := Vector2i(0, 0)
@@ -39,16 +42,50 @@ func _move(dir: Vector2i):
 	print("moved to: ", _tile_position)
 	position += Vector2(dir * GlobalsInst.tile_size_pixels)
 	just_moved.emit(_tile_position)
-	$Sprite.position -= Vector2(dir * GlobalsInst.tile_size_pixels)
+	sprite.position -= Vector2(dir * GlobalsInst.tile_size_pixels)
 
 	if _movement_tween:
 		_movement_tween.kill()
 	_movement_tween = create_tween()
 	_movement_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	var is_sprinting: bool = Input.is_action_pressed("sprint")
+	_play_move_animation(dir, is_sprinting)
 	_movement_tween.tween_property(
-		$Sprite,
+		sprite,
 		"position",
 		0.5 * GlobalsInst.tile_size_pixels,
 		sprinting_movement_time if is_sprinting else movement_time
 	).set_trans(Tween.TRANS_SINE)
+	await _movement_tween.finished
+	if sprite.is_playing():
+		await sprite.animation_finished
+	_play_idle_animation(dir)
+
+
+func _play_move_animation(dir: Vector2i, is_sprinting: bool = false) -> void:
+	match dir:
+		Vector2i.LEFT:
+			sprite.play("motion_left")
+		Vector2i.RIGHT:
+			sprite.play("motion_right")
+		Vector2i.UP:
+			sprite.play("motion_up")
+		Vector2i.DOWN:
+			sprite.play("motion_down")
+	sprite.speed_scale = (
+		sprite.sprite_frames.get_frame_count(sprite.animation)
+		/ sprite.sprite_frames.get_animation_speed(sprite.animation)
+		/ (movement_time)
+	)
+
+
+func _play_idle_animation(dir: Vector2i) -> void:
+	match dir:
+		Vector2i.LEFT:
+			sprite.play("idle_left")
+		Vector2i.RIGHT:
+			sprite.play("idle_right")
+		Vector2i.UP:
+			sprite.play("idle_up")
+		Vector2i.DOWN:
+			sprite.play("idle_down")
